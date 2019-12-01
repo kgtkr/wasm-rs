3a150ee4cfec11d0b1b139115db813b966f2e1b1
kgtkr <kgtkr.jp@gmail.com>
2019-12-01 20:50:30 +0900

    バグ修正

--- src/spec/mod.rs
+++ src/spec/mod.rs
@@ -1,12 +1,15 @@
 use crate::binary::Decoder;
 use crate::exec::instance::{ModuleInst, Val};
 use crate::structure::modules::Module;
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use num::BigInt;
 use num_traits::cast::FromPrimitive;
 use num_traits::cast::ToPrimitive;
 use serde_json::Value;
+use std::io::Cursor;
 use std::str::FromStr;
 
+#[derive(Debug)]
 pub enum Cmd {
     Module(String),
     AssertReturn {
@@ -30,40 +33,36 @@ impl Cmd {
             .collect::<Vec<_>>()
     }
 
+    fn big_to_i32(big: BigInt) -> i32 {
+        (big.clone() & BigInt::from_i32(i32::max_value()).unwrap())
+            .to_i32()
+            .unwrap()
+    }
+
+    fn big_to_i64(big: BigInt) -> i64 {
+        (big.clone() & BigInt::from_i64(i64::max_value()).unwrap())
+            .to_i64()
+            .unwrap()
+    }
+
     fn json_to_value(json: &Value) -> Val {
-        println!("{:?}", json);
         let json_obj = json.as_object().unwrap();
+        let big = BigInt::from_str(json_obj.get("value").unwrap().as_str().unwrap()).unwrap();
         match json_obj.get("type").unwrap().as_str().unwrap() {
-            "i32" => Val::I32(
-                (BigInt::from_str(json_obj.get("value").unwrap().as_str().unwrap()).unwrap()
-                    & BigInt::from_i32(i32::max_value()).unwrap())
-                .to_i32()
-                .unwrap(),
-            ),
-            "i64" => Val::I64(
-                (BigInt::from_str(json_obj.get("value").unwrap().as_str().unwrap()).unwrap()
-                    & BigInt::from_i64(i64::max_value()).unwrap())
-                .to_i64()
-                .unwrap(),
-            ),
-            "f32" => Val::F32(
-                json_obj
-                    .get("value")
-                    .unwrap()
-                    .as_str()
-                    .unwrap()
-                    .parse()
-                    .unwrap(),
-            ),
-            "f64" => Val::F64(
-                json_obj
-                    .get("value")
-                    .unwrap()
-                    .as_str()
-                    .unwrap()
-                    .parse()
-                    .unwrap(),
-            ),
+            "i32" => Val::I32(Cmd::big_to_i32(big)),
+            "i64" => Val::I64(Cmd::big_to_i64(big)),
+            "f32" => {
+                let mut buf = Vec::new();
+                buf.write_i32::<LittleEndian>(Cmd::big_to_i32(big)).unwrap();
+                let mut rdr = Cursor::new(buf);
+                Val::F32(rdr.read_f32::<LittleEndian>().unwrap())
+            }
+            "f64" => {
+                let mut buf = Vec::new();
+                buf.write_i64::<LittleEndian>(Cmd::big_to_i64(big)).unwrap();
+                let mut rdr = Cursor::new(buf);
+                Val::F64(rdr.read_f64::<LittleEndian>().unwrap())
+            }
             _ => panic!(),
         }
     }
@@ -130,9 +129,9 @@ pub fn run_test(filename: String, suc_c: &mut i32, err_c: &mut i32) {
     let mut module: Option<Module> = None;
 
     for cmd in &cmds {
+        println!("[begin]{:?}", cmd);
         match cmd {
             Cmd::Module(name) => {
-                println!("[begin module]{}", name);
                 module = Some(
                     Module::decode_end(&std::fs::read(format!("spec/{}", name)).unwrap()).unwrap(),
                 );
@@ -143,7 +142,6 @@ pub fn run_test(filename: String, suc_c: &mut i32, err_c: &mut i32) {
                 args,
                 expected,
             } => {
-                println!("[begin assert-return]{}", name);
                 match std::panic::catch_unwind(|| {
                     run_assert(module.as_ref().unwrap(), name, args, expected);
                 }) {
@@ -157,7 +155,7 @@ pub fn run_test(filename: String, suc_c: &mut i32, err_c: &mut i32) {
                     }
                 }
             }
-            Cmd::Skip(name) => println!("[skip]{}", name),
+            _ => {}
         }
     }
 }
